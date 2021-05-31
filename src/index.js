ac149e088fdd6a8722d4809e1c4556f9b4b2ea46
daffriedman <dafriedman443@gmail.com>
2021-05-31 18:41:17 +0300

    clock using class based comp with state and setState

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+class Clock extends React.Component {
+  state = { time: "" };
+  componentDidMount() {
+    setInterval(
+      () =>
+        this.setState({
+          time: new Date().toLocaleTimeString(),
+        }),
+      1000
+    );
+  }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  render() {
+    return <div className="time">The time is: {this.state.time}</div>;
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Clock />, document.querySelector("#root"));
